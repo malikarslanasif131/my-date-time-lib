4e8bab904b8c28d5604b30754ed2d68b4fde7638
Malik Arslan Asif <109231105+malikarslanasif131@users.noreply.github.com>
2024-04-01 12:43:56 +0500

    Add files via upload

---
+++ src/DateTime.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MyDateTimeLib;
+
+class DateTime
+{
+    public static function getCurrentDateTime()
+    {
+        return date('Y-m-d H:i:s');
+    }
+}
---
+++ test/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use MyDateTimeLib\DateTime;
+
+echo DateTime::getCurrentDateTime();
